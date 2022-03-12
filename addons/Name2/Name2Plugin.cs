1dfb9a7d7f7ecee00649547649bdb33aa68fa79e
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-03-12 16:28:01 +0800

    Add basic layout

--- addons/Name2/Name2Plugin.cs
+++ addons/Name2/Name2Plugin.cs
@@ -5,7 +5,13 @@ using Godot;
 
 [Tool]
 public class Name2Plugin : EditorPlugin {
+    public override void _EnterTree() {
+        base._EnterTree();
+    }
 
+    public override void _ExitTree() {
+        base._ExitTree();
+    }
 }
 
 #endif // TOOLS
---
+++ addons/Name2/Scripts/SelectableLabel.cs
@@ -0,0 +1,67 @@
+using Godot;
+using System;
+
+public class SelectableLabel : HBoxContainer {
+    private bool selected = false;
+    private bool uppercase = false;
+    private string text = "";
+
+    [Export]
+    private string selectKey = "name_select";
+
+    [Export]
+    public bool Selected {
+        get { return selected; }
+        set { 
+            selected = value;
+            if (selectTexture != null) {
+                float alpha = (selected) ? 1.0f : 0.0f;
+                selectTexture.Modulate = new Color(1.0f, 1.0f, 1.0f, alpha);
+            }
+        }
+    }
+
+    [Export]
+    public bool Uppercase {
+        get { return uppercase; }
+        set { 
+            uppercase = value;
+            if (label != null) {
+                label.Uppercase = value;
+            }
+        }
+    }
+
+    [Export]
+    public string Text {
+        get { return text; }
+        set {
+            text = value;
+            if (label != null) {
+                label.Text = value;
+            }
+        }
+    }
+
+    [Signal]
+    delegate void OnSelected(string val);
+
+    private TextureRect selectTexture = null;
+    private Label label = null;
+
+    public override void _Ready() {
+        selectTexture = GetNode<TextureRect>("Selected");
+        label = GetNode<Label>("Label");
+
+        float alpha = (selected) ? 1.0f : 0.0f;
+        selectTexture.Modulate = new Color(1.0f, 1.0f, 1.0f, alpha);
+        label.Uppercase = uppercase;
+        label.Text = text;
+    }
+
+    public override void _Process(float delta) {
+        if (selected && Input.IsActionJustPressed(selectKey)) {
+            this.EmitSignal("OnSelected", label.Text);
+        }
+    }
+}
