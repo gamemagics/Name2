064855f0e0f33a8243fb8c064f0fab4e1264c3f9
NeilKleistGao <neilkleistgao@bupt.edu.cn>
2022-03-12 18:42:44 +0800

    Add key movement

--- addons/Name2/Scripts/NameNode.cs
+++ addons/Name2/Scripts/NameNode.cs
@@ -1,4 +1,5 @@
 using Godot;
+using Godot.Collections;
 using System;
 
 public class NameNode : VBoxContainer {
@@ -9,6 +10,23 @@ public class NameNode : VBoxContainer {
     private TextureRect avatarRect = null;
     private BackgroundLabel inputLabel = null;
     private BackgroundLabel hintLabel = null;
+    private GridContainer keyboardContainer = null;
+    private HBoxContainer controlContainer = null;
+    private Array<SelectableLabel> keys = new Array<SelectableLabel>();
+    private Array<SelectableLabel> controls = new Array<SelectableLabel>();
+    private int position = 0;
+
+    [Export]
+    private string upKey = "name_up";
+
+    [Export]
+    private string downKey = "name_down";
+
+    [Export]
+    private string leftKey = "name_left";
+
+    [Export]
+    private string rightKey = "name_right";
 
     [Export]
     public Texture Avatar {
@@ -54,5 +72,80 @@ public class NameNode : VBoxContainer {
         avatarRect.Texture = avatar;
         inputLabel.MaxLength = maxLength;
         hintLabel.Text = hint;
+
+        keyboardContainer = GetNode<GridContainer>("KeyboardContainer/SeperatorContainer/InputContainer");
+        controlContainer = GetNode<HBoxContainer>("KeyboardContainer/SeperatorContainer/ControlContainer");
+
+        var keyNodes = keyboardContainer.GetChildren();
+        foreach (var keyNode in keyNodes) {
+            SelectableLabel label = keyNode as SelectableLabel;
+            label.Connect("OnSelected", this, "OnSelected");
+            keys.Add(label);
+        }
+
+        var controlNodes = controlContainer.GetChildren();
+        foreach (var controlNode in controlNodes) {
+            SelectableLabel label = controlNode as SelectableLabel;
+            label.Connect("OnSelected", this, "OnSelected");
+            controls.Add(label);
+        }
+    }
+
+    public override void _Process(float delta) {
+        int next = position;
+        if (Input.IsActionJustPressed(upKey)) {
+            if (position < 0) {
+                int sub = keyboardContainer.Columns / controls.Count;
+                next = keys.Count - (controls.Count + position + 1) * sub;
+            }
+            else if (position >= keyboardContainer.Columns) {
+                next -= keyboardContainer.Columns;
+            }
+        }
+        else if (Input.IsActionJustPressed(downKey)) {
+            if (position > -1 && position < keys.Count - keyboardContainer.Columns) {
+                next += keyboardContainer.Columns;
+            }
+            else if (position >= keys.Count - keyboardContainer.Columns) {
+                int sub = keyboardContainer.Columns / controls.Count;
+                next = -((position % keyboardContainer.Columns) / sub) - 1;
+            }
+        }
+        else if (Input.IsActionJustPressed(leftKey)) {
+            if (position > -1 && position % keyboardContainer.Columns > 0) {
+                --next;
+            }
+            else if (position < -1) {
+                ++next;
+            }
+        }
+        else if (Input.IsActionJustPressed(rightKey)) {
+            if (position > -1 && (position + 1) % keyboardContainer.Columns > 0) {
+                ++next;
+            }
+            else if (position < 0 && position > -controls.Count) {
+                --next;
+            }
+        }
+
+        if (next != position) {
+            SwitchSelectableLabel(position, false);
+            SwitchSelectableLabel(next, true);
+            position = next;
+        }
+    }
+
+    public void OnSelected(string val) {
+        GD.Print(val);
+    }
+
+    private void SwitchSelectableLabel(int index, bool selected) {
+        if (index > -1) {
+            keys[index].Selected = selected;
+        }
+        else {
+            index = -index - 1;
+            controls[index].Selected = selected;
+        }
     }
 }
